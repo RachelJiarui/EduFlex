42f1e60a36f893ca5feaef8d363a9a3852a8bc11
gabimitchell4 <gabimitchell77@gmail.com>
2024-03-30 13:39:01 -0400

    userINput changes

--- rgky-front-end/src/components/user-input/UserInput.js
+++ rgky-front-end/src/components/user-input/UserInput.js
@@ -1,5 +1,18 @@
 function UserInput() {
 
+    document.getElementById("inputForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+      
+        var userInput = document.getElementById("inputField").value;
+      
+        var processedInput = userInput.toUpperCase();
+
+        var outputDiv = document.getElementById("outputDiv");
+        outputDiv.innerHTML = "<p>User Input: " + userInput + "</p>";
+        outputDiv.innerHTML += "<p>Processed Input: " + processedInput + "</p>";
+      });
+      
+
 }
 
 export default UserInput;
