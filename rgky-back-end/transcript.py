17c85a3ab079702df27a1caebeff9dac01d87278
Kevin Polackal <polackal.k@northeastern.edu>
2024-03-30 14:04:30 -0400

    developed fastAPI and added transcript generation functionality

--- rgky-back-end/transcript.py
+++
@@ -1,6 +0,0 @@
-from youtube_transcript_api import YouTubeTranscriptApi
-
-video_id = "xL1OwgVHOww"
-transcript_data = YouTubeTranscriptApi.get_transcript(video_id)
-transcript = " ".join(item['text'] for item in transcript_data).replace('\n', ' ').replace('\r', ' ')
-print(transcript)
--- rgky-front-end/src/services/getTranscript.js
+++ rgky-front-end/src/services/getTranscript.js
@@ -1,3 +1,17 @@
-function getTranscript(youTubeLink) {
-
-} // transcript w/ time stamps
+async function getTranscript(youTubeLink) {
+    const match = youTubeLink.match(/v=([^&]+)/);
+    const videoId = match ? match[1] : null;
+    const url = `http://localhost:8000/get-youtube-transcript-with-timestamp/<video_id>?video_id=${videoId}`; // Update the URL as needed
+    console.log(url)
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(data); 
+    } catch (error) {
+      console.error("Failed to fetch item:", error);
+    }
+  }
+    
