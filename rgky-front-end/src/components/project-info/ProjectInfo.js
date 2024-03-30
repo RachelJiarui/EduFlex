1e5a3b346cad264514e0ec059a54772b261c6247
RachelJiarui <81569164+RachelJiarui@users.noreply.github.com>
2024-03-30 14:05:56 -0400

    front end pront-info

--- rgky-front-end/src/components/project-info/ProjectInfo.js
+++ rgky-front-end/src/components/project-info/ProjectInfo.js
@@ -9,7 +9,7 @@ function ProjectInfo({ userInput, implementationDetails, setImplementationDetail
     };
 
     handleUserInputChange();
-  }, [userInput, setImplementationDetails]);
+  }, [userInput]);
 
   return (
     <div>
--- rgky-front-end/src/services/fetchImplementationGPT.js
+++ rgky-front-end/src/services/fetchImplementationGPT.js
@@ -1,5 +1,5 @@
 export async function fetchImplementationGPT(userInput) {
-  // make sure the api requests numbered implementation steps
+  // make sure the api requests implementation in the form of bulleted points
 
 } // return string
 
