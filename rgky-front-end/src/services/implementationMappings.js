93c40732bedbf25cd91d86e51988446887acbe7e
RachelJiarui <81569164+RachelJiarui@users.noreply.github.com>
2024-03-31 11:54:43 -0400

    garbage

--- rgky-front-end/src/services/implementationMappings.js
+++ rgky-front-end/src/services/implementationMappings.js
@@ -1,4 +1,6 @@
 export async function implementationMappings(implementationText,transcript) { // transcript [{text: _, time: [start_time, end_time]}]
+  // implementationText - the GPT response we received, all the bullet points in a single string
+  // transcript - array of {text: _, time: [start_time, end_time]}
   const url = `http://localhost:8000/implementation_mappings/<implementation_text>?implementation_text=${implementationText}/<transcript>=${transcript}`;
     try {
         const response = await fetch(url);
